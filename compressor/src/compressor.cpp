71d60bb77dce43928ac4884620c6d0ec15c8732a
FortrainMas <vanamokanov8@gmail.com>
2025-02-16 11:29:51 +0000

    Working local libraries and cmake

---
+++ compressor/src/compressor.cpp
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <iostream>
+
+#include "libcompressor/libcompressor.hpp"
+
+int main(){
+	std::cout << "works" << std::endl;
+	std::cout << func() << std::endl;
+
+	return 0;
+}
